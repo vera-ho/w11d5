5f1dc4eae882468e2db622ed0fe216d8fd685563
Vera Ho <vh71886@gmail.com>
2022-06-10 17:00:30 -0400

    finished store

--- pokedex_skeleton/frontend/pokedex.jsx
+++ pokedex_skeleton/frontend/pokedex.jsx
@@ -3,10 +3,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { receiveAllPokemon } from './actions/pokemon_actions';
 import { fetchAllPokemon } from './util/api_util'
+import configureStore  from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
   const rootEl = document.getElementById('root');
-  window.receiveAllPokemon = receiveAllPokemon;
-  window.fetchAllPokemon = fetchAllPokemon
+  // window.receiveAllPokemon = receiveAllPokemon;
+  // window.fetchAllPokemon = fetchAllPokemon
+  const store = configureStore();
+  window.store = store
+  window.getState = store.getState
+  window.dispatch = store.dispatch
   ReactDOM.render(<h1>Pokedex</h1>, rootEl);
+
 });
--- pokedex_skeleton/frontend/reducers/pokemon_reducer.js
+++ pokedex_skeleton/frontend/reducers/pokemon_reducer.js
@@ -1,8 +1,9 @@
 import { RECEIVE_ALL_POKEMON } from '../actions/pokemon_actions';
 
-export default pokemonReducer = (state = {}, action) => {
+export const pokemonReducer = (state = {}, action) => {
     Object.freeze(state);
     let nextState = Object.assign({}, state);
+
     switch(action.type) {
         case RECEIVE_ALL_POKEMON:
             nextState = Object.assign({}, action.pokemon);
