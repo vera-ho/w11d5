32e170148f9d4e5e3486d88ac5eb68225e5a85c6
Vera Ho <vh71886@gmail.com>
2022-06-10 15:43:11 -0400

    pokedexed

---
+++ pokedex_skeleton/app/controllers/api/pokemon_controller.rb
@@ -0,0 +1,10 @@
+class Api::PokemonController < ApplicationController
+    def index
+        @pokemons = Pokemon.all
+        render :index
+    end
+
+    def show
+        @pokemon = Pokemon.find_by(id: params[:id])
+    end
+end
